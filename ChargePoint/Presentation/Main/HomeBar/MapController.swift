2e3585656b1dba80874d0d8888c5f551bd0a47a7
rasulzadex <javidannr@gmail.com>
2025-02-16 16:40:16 +0400

    Auth UI
    Auth UI ready

---
+++ ChargePoint/Presentation/Main/HomeBar/MapController.swift
@@ -0,0 +1,17 @@
+//
+//  MapController.swift
+//  ChargePoint
+//
+//  Created by Javidan on 16.02.25.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
